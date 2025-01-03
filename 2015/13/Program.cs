b6001be11e75894e85d5dbbd2232452bdcec30f4
innerdrivestudios <info@innerdrivestudios.com>
2025-01-03 10:34:16 +0100

    Improved/simplified 2015/13

--- 2015/13/Program.cs
+++ 2015/13/Program.cs
@@ -9,33 +9,27 @@ string myInput = "Alice would lose 2 happiness units by sitting next to Bob.\r\n
 //Your task: find the optimal seating arrangement to ensure the highest overall dinner table happiness
 
 //Step 1. Process the input into a neighbour vs neighbour to "cost" map, while building a list of unique persons
-
 Dictionary<(string, string), int> neighbours2CostMap = new Dictionary<(string personA, string personB), int>();
 HashSet<string> partyAttendants = new HashSet<string>();
+ConvertInput (neighbours2CostMap, partyAttendants);
 
-ProcessInput ();
+//Step 2. Run both challenges
 
-//Step 2. Get all possible seating arrangements:
+//As is:
 List<List<string>> possibleSeatingArrangements = partyAttendants.ToList().GetPermutations();
+Console.WriteLine("Part 1:" + GetBestConfigurationScore(possibleSeatingArrangements, neighbours2CostMap));
 
-//Step 3. Run both challenges
-Part1();
-Part2();
+//With me added:
+partyAttendants.Add("Me");
+possibleSeatingArrangements = partyAttendants.ToList().GetPermutations();
+
+Console.WriteLine("Part 2:" + GetBestConfigurationScore(possibleSeatingArrangements, neighbours2CostMap));
+
 Console.ReadKey();
 
-void Part1()
-{
-    Console.WriteLine("Part 1:"+GetBestConfigurationScore());
-}
+//////////////////////////////////// HELPER METHODS ///////////////////////////////////////
 
-void Part2()
-{
-    partyAttendants.Add("Me");
-    possibleSeatingArrangements = partyAttendants.ToList().GetPermutations();
-    Console.WriteLine("Part 2:" + GetBestConfigurationScore());
-}
-
-void ProcessInput ()
+void ConvertInput (Dictionary<(string, string), int> pCostmap, HashSet<string> pAttendants)
 {
     string pattern = @"(\w+) would (lose|gain) (\d+) happiness units by sitting next to (\w+)\.\r\n";
     MatchCollection matches = Regex.Matches(myInput, pattern);
@@ -44,45 +38,44 @@ void ProcessInput ()
     {
         if (match.Success)
         {
-
             string personA = match.Groups[1].Value;
             string personB = match.Groups[4].Value;
             string action = match.Groups[2].Value;
             int value = int.Parse(match.Groups[3].Value);
 
-            neighbours2CostMap[(personA, personB)] = (action == "gain") ? value : -value;
-            partyAttendants.Add(personA);
+            pCostmap[(personA, personB)] = (action == "gain") ? value : -value;
+            pAttendants.Add(personA);
         }
     }
 }
 
-int GetBestConfigurationScore()
+int GetBestConfigurationScore(List<List<string>> pPossibleSeatingArrangements, Dictionary<(string, string), int> pCostmap)
 {
     int bestConfiguration = int.MinValue;
 
-    foreach (List<string> seatingArrangement in possibleSeatingArrangements)
+    foreach (List<string> seatingArrangement in pPossibleSeatingArrangements)
     {
-        int score = GetResult(seatingArrangement);
+        int score = GetResult(seatingArrangement, pCostmap);
         bestConfiguration = Math.Max(bestConfiguration, score);
     }
 	
 	return bestConfiguration;
 }
 
-int GetResult (List<string> pSeatingArrangement)
+int GetResult (List<string> pSeatingArrangement, Dictionary<(string, string), int> pCostmap)
 {
 	int total = 0;
 	for (int i = 0; i < pSeatingArrangement.Count; i++)
 	{
-        //If there are people in the attendance list who are not in the map just use a score of zero for them (aka skip 'm)
         string personA = pSeatingArrangement[i];
         string personB = pSeatingArrangement[(i + 1) % pSeatingArrangement.Count];
 
-        if (personA == "Me" || personB == "Me") continue;
+        //If there are people in the attendance list who are not in the map just use a score of zero for them (aka skip 'm)
+        if (!pCostmap.ContainsKey ((personA, personB)) || !pCostmap.ContainsKey((personB, personA))) continue;
 
         //Don't use exceptions to catch invalid items, since it would be very slow
-	    total += neighbours2CostMap[(personA, personB)];
-	    total += neighbours2CostMap[(personB, personA)];
+	    total += pCostmap[(personA, personB)];
+	    total += pCostmap[(personB, personA)];
     }
 
 	return total;

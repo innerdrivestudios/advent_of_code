da59b4797e9b01bf6a668d55bf1268c775271434
innerdrivestudios <info@innerdrivestudios.com>
2025-01-13 17:06:02 +0100

    Added 2015/24

---
+++ 2015/24/Program.cs
@@ -0,0 +1,76 @@
+﻿//Solution for https://adventofcode.com/2015/day/24 (Ctrl+Click in VS to follow link)
+
+//Input: list of package weights
+
+string myInput = "1\r\n3\r\n5\r\n11\r\n13\r\n17\r\n19\r\n23\r\n29\r\n31\r\n37\r\n41\r\n43\r\n47\r\n53\r\n59\r\n67\r\n71\r\n73\r\n79\r\n83\r\n89\r\n97\r\n101\r\n103\r\n107\r\n109\r\n113";
+//string myInput = "11\r\n10\r\n9\r\n8\r\n7\r\n5\r\n4\r\n3\r\n2\r\n1";
+
+//Task: create 3 different groups of packages of equal weights, with group 1 having the least amount of packages
+
+//First get all the numbers and make sure they are sorted to speed up the algorithm
+List<int> packages = myInput.Split("\r\n", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+packages.Sort((a,b) => b-a);
+
+Console.WriteLine("Part 1: Quantum entanglement of the ideal config:" + CalculateIdealQuantumEntanglement(packages, 3));
+Console.WriteLine("Part 2: Quantum entanglement of the ideal config:" + CalculateIdealQuantumEntanglement(packages, 4));
+
+Console.ReadKey();
+
+long CalculateIdealQuantumEntanglement(List<int> pPackages, int pNumberOfGroups)
+{
+    //Getting all permutations and testing those takes too much time, so we use a smarter approach
+    List<List<int>> possibleConfigurations = new List<List<int>>();
+    GetPossibleConfigurationsOfWeightX(pPackages, pPackages.Sum() / pNumberOfGroups, possibleConfigurations);
+
+    //Find the minimum package length
+    int minPackageLength = possibleConfigurations.Min (a => a.Count);
+    possibleConfigurations = possibleConfigurations.Where (a => a.Count == minPackageLength).ToList();
+
+    //Find optimum quantum entanglement score ;)
+    long optimumQuantumScore = possibleConfigurations.Min(a => CalculateQuantumEntanglement(a));
+
+	return optimumQuantumScore;
+}
+
+//also see the worked_example.txt
+//This method expects a descending sorted list of package weights, so it can go through all the provided
+//packages in an order fashion in order to try and establish the requested weight.
+
+void GetPossibleConfigurationsOfWeightX (List<int> pPackages, int pWeight, List<List<int>> pFoundConfigurations, List<int> pConfigurationSoFar = null)
+{
+    pConfigurationSoFar = pConfigurationSoFar ?? new List<int>();
+
+    //See if we can even make the requested configuration by combining everything so far, 
+    //otherwise do an early exit
+    int maxWeight = pPackages.Sum() + pConfigurationSoFar.Sum();
+    if (maxWeight < pWeight) return;
+
+    //Console.WriteLine(String.Join(" ", pConfigurationSoFar));
+    //Ok, so in theory by combining items from pPackages and pConfigurationSoFar we should be able to
+    //get to pWeight, the question is, can we get to it EXACTLY
+
+    for (int i = 0; i < pPackages.Count; i++)
+    {
+        List<int> configurationSoFarClone = new List<int>(pConfigurationSoFar) { pPackages[i] };
+
+        int total = configurationSoFarClone.Sum();
+
+        if (total == pWeight)
+        {
+            pFoundConfigurations.Add(configurationSoFarClone);
+        }
+        else if (total < pWeight)
+        {
+            List<int> subPackages = pPackages.Slice(i+1, pPackages.Count-(i+1));
+            GetPossibleConfigurationsOfWeightX(subPackages, pWeight, pFoundConfigurations, configurationSoFarClone);
+        }
+        //else total > pWeight, which is a bust so we skip it
+    }
+}
+
+long CalculateQuantumEntanglement (List<int> pWeights)
+{
+    long result = 1;
+    foreach (int weight in pWeights) result *= weight;
+    return result;
+}
--- ListExtensions.cs
+++ ListExtensions.cs
@@ -35,4 +35,43 @@ public static class ListExtensions
         }
     }
 
+    public static List<List<T>> GetPermutationsIterative<T>(this List<T> pList)
+    {
+		List<List<T>> result = new List<List<T>>();
+		result.Add(pList);
+
+		//how many times do we have to loop? Given 1,2,3,4
+		//We start by cloning 1,2,3,4 and swapping every element 1vs1, 1vs2, 1vs3, 1vs4
+		//Then we take the result of that (4 permutations), and start at the 2nd element,etc
+		//We can skip the last element because that is just causes a swap of the 4th vs the 4th element in every list
+		for (int i = 0; i < pList.Count - 1; i++)
+		{
+			//we need to loop through the current amount of permutations we want to process
+			//since every step that processes a permutation results in a bunch of new ones, 
+			//we need to make sure the for loop doesn't take them into account yet
+			int currentCount = result.Count;
+			for (int j = 0; j < currentCount; j++)
+			{
+				//for the amount of permutations to process, we keep 'popping' one 
+				//and then we process that permutation which means that for every possible swap,
+				//we create a copy of our popped permutation, perform the required swap and add it back to the list for the go around
+				//(performance could be improved, but this is just an example)
+				List<T> listToProcess = result[0];
+				result.RemoveAt(0);
+
+				for (int k = i; k < pList.Count; k++)
+				{
+					List<T> newList = new List<T>(listToProcess);
+					T tmp = newList[i];
+					newList[i] = newList[k];
+					newList[k] = tmp;
+					result.Add(newList);
+				}
+			}
+		}
+
+		return result;
+	}
+
+
 }

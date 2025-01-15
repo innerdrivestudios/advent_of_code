4e5193070edfccc077c36dbb2c0ed90304778f68
innerdrivestudios <info@innerdrivestudios.com>
2025-01-15 12:06:26 +0100

    Added 2016/02

--- Vec2.cs
+++ Vec2.cs
@@ -46,7 +46,17 @@ public struct Vec2<T> where T: INumber<T>
 		return new Vec2<T>(-a.X, -a.Y);
 	}
 
-	public override string ToString()
+    public static bool operator ==(Vec2<T> a, Vec2<T> b)
+    {
+        return a.X == b.X && a.Y == b.Y;
+    }
+
+    public static bool operator !=(Vec2<T> a, Vec2<T> b)
+    {
+        return !(a == b);
+    }
+
+    public override string ToString()
     {
         return $"({X},{Y})";
     }

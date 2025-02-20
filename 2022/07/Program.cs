28e9e64da7c562e7bac74bc79015cf176c52e106
innerdrivestudios <info@innerdrivestudios.com>
2025-02-20 22:24:58 +0100

    Added 2022/07

---
+++ 2022/07/Program.cs
@@ -0,0 +1,85 @@
+﻿// Solution for https://adventofcode.com/2022/day/7 (Ctrl+Click in VS to follow link)
+
+// In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties
+// and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable
+
+// ** Your input: a list of semi linux instructions
+
+using System.IO;
+
+string myInput = File.ReadAllText(args[0]);
+
+string[] consoleLog = myInput
+	.ReplaceLineEndings(Environment.NewLine)
+	.Split (Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+	.ToArray();
+
+// ** Step 1: Process the whole console log and build the directory tree.
+
+// What are some of our options for building the directory tree?
+// We could use a dictionary from string to fileinfo (type, size, name, List<string> children)
+// But this might result in conflicts, if unrelated nested directories have duplicate names.
+
+// Instead we'll implement a Node class that can have child nodes and a parent node,
+// so that we can create a node structure, while moving up and down the directory hierarchy
+// based on the commands we are "inputting", and the results the log is outputting...
+
+// Static method to get started with a single root node ...
+TreeNode current = TreeNode.GetRoot();
+
+// Now parse the whole console log ...
+
+foreach (string consoleLine in consoleLog)
+{
+	if (consoleLine.StartsWith("$ cd "))
+	{
+		string cdArgument = consoleLine.Substring("$ cd ".Length);
+		current = current.GetNode(cdArgument);
+	}
+	else if (consoleLine.StartsWith("$ ls"))
+	{
+		//ignore ...
+	}
+	else if (consoleLine.StartsWith("dir"))
+	{
+		string directory = consoleLine.Substring("dir ".Length);
+		current.CreateNode(directory);
+	}
+	else // the only option left
+	{
+		string[] fileParts = consoleLine.Split(" ");
+
+		int fileSize = int.Parse(fileParts[0]);
+		string fileName = fileParts[1];
+
+		current.CreateNode(fileName, fileSize);
+	}
+}
+
+// Now that we've parsed the whole console log, we can print the tree to see if we did it correctly
+// current.GetNode("/").Print();
+
+// ** Part 1 - Find all of the directories with a total size of at most 100000,
+// then calculate the sum of their total sizes.
+
+TreeNode root = current.GetNode("/");
+root.CalculateDirectorySizes();
+
+Console.WriteLine("Part 1 - Requested sum: " + root.TotalSumOfAllDirectoriesBelow (100000));
+
+// ** Part 2 -
+// - Find a directory to delete so that we have at 30000000 free space.
+// - The total space available to the file system is 70000000
+
+int totalDiscSpace = 70000000;
+int totalSpaceRequired = 30000000;
+int totalSpaceTaken = root.size;
+int totalSpaceFree = totalDiscSpace - totalSpaceTaken;
+int spaceToFreeUp = totalSpaceRequired - totalSpaceFree;
+
+// Console.WriteLine("Part 2 - Space left: " + totalSpaceFree);
+// Console.WriteLine("Part 2 - Space to delete: " + spaceToFreeUp);
+
+TreeNode directoryToDelete = root.FindSmallestDirectorySized(spaceToFreeUp);
+Console.WriteLine("Part 2 - Directory size to delete:" + directoryToDelete.size);
---
+++ 2022/07/TreeNode.cs
@@ -0,0 +1,143 @@
+﻿/**
+ * Basic tree node class that allows easy up and down traversal of its children.
+ */
+class TreeNode
+{
+	public readonly Dictionary<string, TreeNode> children = new();
+	public TreeNode parent { get; private set; }	
+
+	public string name { get; private set; } 
+	public int size { get; private set; }
+	public bool isDirectory => children.Count > 0;
+
+	private static TreeNode root = null;
+
+	public static TreeNode GetRoot ()
+	{
+		if (root == null) root = new TreeNode("/");
+		return root;
+	}
+
+	private TreeNode (string pName, int pSize = 0)
+	{
+		name = pName;
+		size = pSize;
+	}
+
+	public TreeNode CreateNode(string pName, int pSize = 0)
+	{
+		TreeNode childNode = new TreeNode(pName, pSize);
+		children[pName] = childNode;
+		childNode.parent = this;
+		return childNode;
+	}
+
+	public TreeNode GetNode(string pName)
+	{
+		if (pName == "/")
+		{
+			return root;
+		}
+		else if (pName == "..")
+		{
+			return parent;
+		}
+		else
+		{
+			return children[pName];
+		}
+	}
+
+	// To calculate the directory sizes, we'll need to sum the size of all of its children recursively
+	public int CalculateDirectorySizes()
+	{
+		if (isDirectory)
+		{
+			int childSize = 0;
+			
+			foreach (var child in children.Values)
+			{
+				childSize += child.CalculateDirectorySizes();
+			}
+
+			//Store the size of all our children as our size...
+			size = childSize;
+
+			return childSize;
+		}
+		else
+		{
+			// Simply return our own size
+			return size;
+		}
+	}
+
+	public int TotalSumOfAllDirectoriesBelow (int pCutOffValue)
+	{
+		int totalSum = 0;
+
+		if (isDirectory && size <= pCutOffValue) totalSum += size;
+
+		foreach (var child in children.Values)
+		{
+			if (child.isDirectory) //Small optimization, we can also leave it out...
+			{
+                totalSum += child.TotalSumOfAllDirectoriesBelow(pCutOffValue);
+			}
+		}
+
+		return totalSum;
+	}
+
+	public TreeNode FindSmallestDirectorySized(int pSize)
+	{
+		if (!isDirectory) return null;
+
+		TreeNode smallestNodeAbove = null;
+			
+		// First check if any child directory (which is always smaller than us)
+		// can match the requested size ...
+		foreach (var child in children.Values)
+		{ 
+			if (!child.isDirectory) continue;
+			
+			// Get the smallest matching directory amongst our children
+			TreeNode smallestMatchingChildNode = 
+				child.FindSmallestDirectorySized(pSize);
+
+			// None found? 
+			if (smallestMatchingChildNode == null) continue;
+
+			// One found?
+			if (
+				smallestNodeAbove == null || 
+				smallestMatchingChildNode.size < smallestNodeAbove.size
+				)
+			{
+				smallestNodeAbove = smallestMatchingChildNode;
+ 			}
+		}
+
+		// If our size is bigger than the requested size and we haven't found 
+		// a smaller child node yet...
+		if (size >= pSize && smallestNodeAbove == null) smallestNodeAbove = this;
+		
+		return smallestNodeAbove;
+	}
+
+	public void Print()
+	{
+		InnerPrint(0);
+	}
+
+	private void InnerPrint(int pDepth)
+	{
+		Console.WriteLine(new String('\t', pDepth) + name + "(" + size + ")");
+
+		foreach (TreeNode childNode in children.Values)
+		{
+			childNode.InnerPrint(pDepth + 1);
+		}
+	}
+
+}

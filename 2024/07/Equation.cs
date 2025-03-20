8b253c1e3f985f9d05088fc37a719bce7d6d92f0
innerdrivestudios <info@innerdrivestudios.com>
2025-03-20 20:08:20 +0100

    Added 2024/07

---
+++ 2024/07/Equation.cs
@@ -0,0 +1,69 @@
+﻿class Equation
+{
+	public long result;
+	public long[] operands;
+
+	public Equation(string equationString)
+	{
+		// 190: 10 19
+		string[] equationParts = equationString.Split(": ");
+		result = long.Parse(equationParts[0]);
+		operands = equationParts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select (long.Parse).ToArray();
+	}
+
+	public bool EvaluatePart1()
+	{
+		int operatorCount = operands.Length-1;
+		//2 operands? +/* +/* = 4
+		int variations = (int)Math.Pow(2, operatorCount);
+
+		for (int v = 0; v < variations; v++)
+		{
+			long total = operands[0];
+			for (int i = 0; i < operatorCount; i++)
+			{
+				if ((v & 1 << i) == 0) 
+				{ 
+					total += operands[i + 1]; 
+				}
+				else
+				{
+					total *= operands[i + 1]; 
+				}
+			}
+			if (total == result) return true;
+        }
+		return false;
+	}
+
+	public bool EvaluatePart2()
+	{
+		int operatorCount = operands.Length - 1;
+		
+		//+/*/|| (3) => 3 * 3 * ... 
+		// 0    1    2		3		4     5			6     7     8     
+		// ++   +*   +||    *+		**	  *||		||+	  ||*	||||
+		int variations = (int)Math.Pow(3, operatorCount);
+
+		for (int v = 0; v < variations; v++)
+		{
+			long total = operands[0];
+			int testNumber = v;
+
+			for (int i = 0; i < operatorCount; i++)
+			{
+				//so every value we testing needs to go 0,1,2,0,1,2
+
+                if (testNumber%3 == 0) { total += operands[i + 1]; }
+				else if (testNumber%3 == 1) { total *= operands[i + 1]; }
+				else if (testNumber%3 == 2) { total = long.Parse("" + total + operands[i + 1]);}
+
+				//in addition every next operand is basically a bit shift
+				testNumber /= 3;
+			}
+			if (total == result) return true;
+		}
+		return false;
+	}
+
+}
---
+++ 2024/07/Program.cs
@@ -0,0 +1,48 @@
+﻿//Solution for https://adventofcode.com/2024/day/7 (Ctrl+Click in VS to follow link)
+
+// In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties
+// and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable
+
+// ** Your input: a list of numbers followed by a list of numbers you need to try and make them with
+
+string myInput = File.ReadAllText(args[0]);
+myInput = myInput.ReplaceLineEndings(Environment.NewLine);
+
+// ** Parse the input
+
+string[] equations = myInput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+List<Equation> equationsList = new List<Equation>();
+
+foreach (string equation in equations)
+{
+	equationsList.Add(new Equation(equation));
+}
+
+// ** Part 1
+
+long total = 0;
+foreach (Equation equation in equationsList)
+{
+	if (equation.EvaluatePart1())
+	{
+		total += equation.result;
+	}
+}
+
+Console.WriteLine("Part 1:" + total);
+
+// ** Part 2
+
+total = 0;
+foreach (Equation equation in equationsList)
+{
+	if (equation.EvaluatePart2())
+	{
+		total += equation.result;
+	}
+}
+
+Console.WriteLine("Part 2:" + total);
+

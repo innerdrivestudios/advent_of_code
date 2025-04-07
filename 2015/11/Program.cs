5ebf1c4921a2592056265ae35f6a16aad74c9186
innerdrivestudios <info@innerdrivestudios.com>
2025-04-07 18:16:47 +0200

    Added 2015/11

--- 2015/11/Program.cs
+++ 2015/11/Program.cs
@@ -1,21 +1,28 @@
-﻿//Solution for https://adventofcode.com/2015/day/11 (Ctrl+Click in VS to follow link)
+﻿// Solution for https://adventofcode.com/2015/day/11 (Ctrl+Click in VS to follow link)
 
-//Your input: Santa's current password
-string myInput = "vzbxkghb";
+// In visual studio you can modify the char sequence used by going to
+// Debug/Debug Properties and changing the command line arguments.
+// This value given will be passed to the built-in args[0] variable.
 
-//Your task: find the next valid password by 'incrementing' chars like this xx, xy, xz, ya, yb etc matching these requirements:
-//1. Passwords must include one increasing straight of at least three letters, like abc, bcd, cde, and so on, up to xyz.
-//   They cannot skip letters; abd doesn't count.
-//2. Passwords may not contain the letters i, o, or l, as these letters can be mistaken for other characters and are therefore confusing.
-//3. Passwords must contain at least two different, non-overlapping pairs of letters, like aa, bb, or zz.
+// ** Your input: Santa's current password
+string myInput = args[0];
+
+// ** Part 1: Find the next valid password by 'incrementing' chars like this
+//            xx, xy, xz, ya, yb etc matching these requirements:
+//
+//      1. Passwords must include one increasing straight of at least three letters,
+//         like abc, bcd, cde, and so on, up to xyz.
+//         They cannot skip letters; abd doesn't count.
+//      2. Passwords may not contain the letters i, o, or l,
+//         as these letters can be mistaken for other characters and are therefore confusing.
+//      3. Passwords must contain at least two different, non-overlapping pairs of letters,
+//         like aa, bb, or zz.
 
 string nextPassword = FindNextValidPassword(myInput);
 Console.WriteLine("Part 1 - Next valid password:"+ nextPassword);
 
 nextPassword = FindNextValidPassword(nextPassword);
 Console.WriteLine("Part 2 - Next valid password:"+ nextPassword);
-
-Console.ReadKey();
 
 string FindNextValidPassword (string pInput)
 {
@@ -41,13 +48,18 @@ void NextPassword(char[] pInputChars)
 
 bool IsCorrectPassword(char[] pPassword)
 {
-	//Passwords may not contain the letters i, o, or l, as these letters can be mistaken for other characters and are therefore confusing.
-	return !ContainsChars(pPassword, [ 'i', 'o', 'l' ])
+	return 
+        
+	//Passwords may not contain the letters i, o, or l,
+    //as these letters can be mistaken for other characters and are therefore confusing.
+    !ContainsChars(pPassword, [ 'i', 'o', 'l' ])
 
-    //Passwords must include one increasing straight of at least three letters, like abc, bcd, cde, and so on, up to xyz. They cannot skip letters; abd doesn't count.
+    //Passwords must include one increasing straight of at least three letters,
+    //like abc, bcd, cde, and so on, up to xyz. They cannot skip letters; abd doesn't count.
     && HasAtLeastOneStraight(pPassword)
 
-    //Passwords must contain at least two different, non - overlapping pairs of letters, like aa, bb, or zz.
+    //Passwords must contain at least two different,
+    //non - overlapping pairs of letters, like aa, bb, or zz.
     && HasAtLeastXTwins (pPassword, 2);
 }
 
@@ -62,10 +74,13 @@ bool HasAtLeastOneStraight(char[] pPassword)
     {
         if (pPassword[i] + 1 == pPassword[i + 1] && pPassword[i + 1] + 1 == pPassword[i + 2])
         {
-            return true; // Found a run of increasing characters
+			// Found a run of increasing characters
+			return true;         
         }
     }
-    return false; // No run of increasing characters found
+
+	// No run of increasing characters found
+	return false; 
 }
 
 bool HasAtLeastXTwins(char[] pPassword, int pRequiredAmountOfTwins)
@@ -74,7 +89,7 @@ bool HasAtLeastXTwins(char[] pPassword, int pRequiredAmountOfTwins)
     
     for (int i = 0; i < pPassword.Length - 1; i++)
     {
-        //if pairs found, increase i additionaly to make sure the twins don't overlap
+        //if pairs found, increase i additionally to make sure the twins don't overlap
         if (pPassword[i] == pPassword[i + 1])
         {
             twinsFound++;

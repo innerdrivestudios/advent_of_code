1ef61545e6dcc384a17ec028d29c3f6e2f2ee4fe
innerdrivestudios <info@innerdrivestudios.com>
2025-04-14 09:01:32 +0200

    Added 2022/11

---
+++ 2022/11/Monkey.cs
@@ -0,0 +1,112 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+/** Parses and operates a monkey based on data like this:
+
+Monkey 0:
+  Starting items: 98, 97, 98, 55, 56, 72
+  Operation: new = old * 13
+  Test: divisible by 11
+    If true: throw to monkey 4
+    If false: throw to monkey 7
+
+*/
+class Monkey
+{
+    public readonly int id;                                      //monkey id
+    public readonly List<long> startingItems;                   //list of starting items as given
+    (char op, long operand) operation;                           // * or + and a number times OLD
+    (long divisibleBy, int trueMonkey, int falseMonkey) test;
+    
+    private static Dictionary<int, Monkey> monkeyMap = new Dictionary<int, Monkey>();
+
+    private long _itemsInspected = 0;
+    public long ItemsInspected => _itemsInspected;
+
+    private long worryLevelDivider = 1;
+
+    //We use this value to modulo any results in order to avoid overflow
+    public static long globalModulo = 1;
+
+    public Monkey (string pMonkeyData, long pWorryLevelDivider)
+    {
+        Regex monkeyParser = new Regex(
+            @"Monkey (\d+):"+Environment.NewLine+                                   //Group 1
+            @"\s+Starting items: (\d+(?:, ?\d+)*)" + Environment.NewLine +          //Group 2
+            @"\s+Operation: new = old ([*+]) (\d+|old)" + Environment.NewLine +     //Group 3 and 4
+            @"\s+Test: divisible by (\d+)" + Environment.NewLine +                  //Group 5
+            @"\s+If true: throw to monkey (\d+)" + Environment.NewLine +            //Group 6
+            @"\s+If false: throw to monkey (\d+)"                                   //Group 7
+        );
+
+        Match match = monkeyParser.Match( pMonkeyData );
+        if (!match.Success) throw new Exception("Invalid monkey data");
+        
+        id = int.Parse(match.Groups[1].Value);
+        startingItems = match.Groups[2].Value.Split(",", StringSplitOptions.TrimEntries).Select(long.Parse).ToList();
+
+        if (match.Groups[4].Value == "old")
+        {
+            operation.op = 'X'; //easiest way to encode this
+        }
+        else
+        {
+            operation.op = match.Groups[3].Value[0];
+            operation.operand = long.Parse (match.Groups[4].Value);
+        }
+
+        test.divisibleBy = long.Parse(match.Groups[5].Value);
+        test.trueMonkey = int.Parse(match.Groups[6].Value);
+        test.falseMonkey = int.Parse(match.Groups[7].Value);
+
+        worryLevelDivider = pWorryLevelDivider;
+
+        //We need to maintain the property that whatever the modulo result,
+        //it still needs to be divisible by our test value
+        globalModulo *= test.divisibleBy;
+
+        //Now we got all monkey data successfully parsed
+        monkeyMap[id] = this;
+    }
+
+    public override string ToString()
+    {
+        string items = string.Join(", ", startingItems);
+        string opStr = $"new = old {operation.op} {operation.operand}";
+        string testStr =
+            $"  Test: divisible by {test.divisibleBy}\n" +
+            $"    If true: throw to monkey {test.trueMonkey}\n" +
+            $"    If false: throw to monkey {test.falseMonkey}";
+
+        return $"Monkey {id}:\n" +
+               $"  Starting items: {items}\n" +
+               $"  Operation: {opStr}\n" +
+               $"{testStr}";
+    }
+
+    public void Inspect()
+    {
+        //Implementing the monkey test attributes according to specs
+        for (int i = startingItems.Count-1; i >= 0; i--)
+        {
+            startingItems[i] = (InspectItem(startingItems[i]) / worryLevelDivider) % (globalModulo);
+            bool testResult = (startingItems[i] % (long)test.divisibleBy) == 0;
+
+            monkeyMap[testResult ? test.trueMonkey : test.falseMonkey].startingItems.Add(startingItems[i]);
+            startingItems.RemoveAt(i);
+
+            _itemsInspected++;
+        }
+    }
+
+    private long InspectItem(long pItem)
+    {
+        if (operation.op == 'X') return pItem * pItem;
+        if (operation.op == '*') return pItem * (long)operation.operand;
+        if (operation.op == '+') return pItem + (long)operation.operand;
+
+        throw new NotImplementedException();
+    }
+    
+}
+
---
+++ 2022/11/Program.cs
@@ -0,0 +1,73 @@
+﻿// Solution for https://adventofcode.com/2022/day/11 (Ctrl+Click in VS to follow link)
+
+// In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties
+// and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable
+
+// ** Your input: a list of monkey information to process
+
+string myInput = File.ReadAllText(args[0]);
+myInput = myInput.ReplaceLineEndings(Environment.NewLine);
+
+string[] monkeyInput = myInput.Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.TrimEntries);
+
+Monkey[] monkeys = monkeyInput.Select (x => new Monkey(x, 3)).ToArray();
+
+// ** Part 1: Count the total number of times each monkey inspects items over 20 rounds
+
+for (int i = 0; i < 20; i++)
+{
+    foreach (var monkey in monkeys) monkey.Inspect();
+}
+
+foreach (var monkey in monkeys)
+{
+    Console.WriteLine("Monkey " + monkey.id + " inspected items "+monkey.ItemsInspected + " times.");
+}
+
+long[] monkeyBusiness = monkeys.Select (x => x.ItemsInspected).OrderByDescending(x => x).Take(2).ToArray();
+Console.WriteLine("Part 1 - Level of monkey business:"+ monkeyBusiness[0] * monkeyBusiness[1]);
+
+// ** Part 2: Same but over 10000 rounds.
+// The issue here is that due to the old * old operation in some of the Monkeys
+// the result numbers overflow a (u)long. Initially this had me stumped for a bit
+// I tried using doubles/floats/Decimals/BigIntegers etc.
+// BigIntegers might work in theory but the numbers become so big, that so many 
+// bits are required that the application slows down to a crawl.
+
+// After sleeping on it for a night I realized the most important thing is to make sure
+// that whatever we do with the worry levels, we need to maintain the property that
+// it cannot change the outcome of the division test.
+
+// In order to do that, creating monkeys updates a globalModulo value, which is
+// updated by multiplying the previous global modulo by the current monkeys
+// division test. (Since they are all prime numbers this will work without further tinkering).
+// Using this value we can make sure any inspection results don't overflow.
+
+// However since creating monkeys for a second time would modify this value a second
+// time we need to reset it back to 1 between runs...
+// (and yes, modifying global value is not the best approach)
+
+Monkey.globalModulo = 1;
+monkeys = monkeyInput.Select(x => new Monkey(x, 1)).ToArray();
+Console.WriteLine(Monkey.globalModulo);
+
+for (int i = 0; i < 10000; i++)
+{
+    //Console.WriteLine();
+    //Console.WriteLine("Round "+(i+1));
+    foreach (var monkey in monkeys)
+    {
+        monkey.Inspect();
+        //Console.WriteLine("Monkey " + monkey.id + " inspected items " + monkey.ItemsInspected + " times");
+    }
+}
+
+foreach (var monkey in monkeys)
+{
+    Console.WriteLine("Monkey " + monkey.id + " inspected items " + monkey.ItemsInspected + " times.");
+}
+
+monkeyBusiness = monkeys.Select(x => x.ItemsInspected).OrderByDescending(x => x).Take(2).ToArray();
+Console.WriteLine("Part 2 - Level of monkey business:" + monkeyBusiness[0] * monkeyBusiness[1]);
+

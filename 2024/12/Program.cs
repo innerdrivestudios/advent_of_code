26e69db92288a48d9574e7819a659834e9cbba52
innerdrivestudios <info@innerdrivestudios.com>
2025-04-23 20:27:39 +0200

    Added 2024/12

---
+++ 2024/12/Program.cs
@@ -0,0 +1,39 @@
+﻿//Solution for https://adventofcode.com/2024/day/12 (Ctrl+Click in VS to follow link)
+
+using Vec2i = Vec2<int>;
+
+// In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties
+// and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable
+
+// ** Your input: a grid with characters that form regions
+
+string myInput = File.ReadAllText(args[0]).ReplaceLineEndings(Environment.NewLine);
+
+// Parsing the input:
+
+Grid<char> grid = new Grid<char>(myInput, Environment.NewLine);
+
+// ** Part 1 & 2: What is the total price of fencing all regions on your map?
+
+// Basically for each position in the grid that hasn't been visited yet,
+// we'll run a region detection algorithm...
+
+HashSet<Vec2i> visited = new();
+List<Region> regions = new List<Region>();
+
+grid.Foreach(
+	(position, id) =>
+	{
+		if (!visited.Contains(position))
+		{
+			HashSet<Vec2i> filledPositions = grid.FloodFill(position, x => grid[x] == id);
+			visited.UnionWith(filledPositions);
+			regions.Add(new Region(filledPositions));
+		}
+	}
+);
+
+Console.WriteLine("Part 1:" + regions.Sum(x => x.area * x.perimeter));
+Console.WriteLine("Part 2:" + regions.Sum(x => x.area * x.GetOptimizedPerimeterSize()));
+
---
+++ 2024/12/Region.cs
@@ -0,0 +1,109 @@
+﻿using Vec2i = Vec2<int>;
+
+class Region
+{
+	public int area => positions.Count;
+	public int perimeter { get; private set; }
+
+	Dictionary<Vec2i, HashSet<Vec2i>> graph = new Dictionary<Vec2i, HashSet<Vec2i>>();
+	private HashSet<Vec2i> positions = new();
+
+	public Region (HashSet<Vec2i> pPositions)
+	{
+		positions = pPositions;
+		RunEdgeDetection();
+    }
+
+	private void RunEdgeDetection()
+	{
+		foreach (Vec2i v in positions)
+		{
+			//build up a grid of points we'll use to walk the outer edge
+			ToggleEdge(v + new Vec2i(0, 0), v + new Vec2i(1, 0));
+			ToggleEdge(v + new Vec2i(1, 0), v + new Vec2i(1, 1));
+			ToggleEdge(v + new Vec2i(1, 1), v + new Vec2i(0, 1));
+			ToggleEdge(v + new Vec2i(0, 1), v + new Vec2i(0, 0));
+		}
+	}
+
+	private void ToggleEdge(Vec2i pNodeA, Vec2i pNodeB)
+	{
+		// Make sure to delete internal edges (every edge that is visited twice)
+		// However edges visited twice are caused by another square which means
+		// the order of the edges will be reversed
+
+		if (graph.TryGetValue(pNodeB, out var existingEdges) && existingEdges.Remove(pNodeA))
+		{
+			perimeter--;
+			if (existingEdges.Count == 0) graph.Remove(pNodeB);
+		}
+		else
+		{
+			var newEdges = graph.GetValueOrDefault(pNodeA, new());
+			newEdges.Add(pNodeB);
+			graph[pNodeA] = newEdges;
+			perimeter++;
+		}
+	}
+
+
+	public int GetOptimizedPerimeterSize()
+	{
+		int totalPerimeter = 0;
+
+		var graphCopy = new Dictionary<Vec2i, HashSet<Vec2i>> (graph);
+
+		while (graphCopy.Count > 0)
+		{
+			Vec2i startPosition = graphCopy.First().Key;
+			Vec2i currentPosition = startPosition;
+			Vec2i lastDirection = new Vec2i(-1000, -1);
+
+			int count = 0;
+
+			do
+			{
+				HashSet<Vec2i> connections = graphCopy[currentPosition];
+				Vec2i nextPoint = connections.First();
+				connections.Remove(nextPoint);
+
+				if (connections.Count == 0)
+				{
+					graphCopy.Remove(currentPosition);
+				}
+
+				Vec2i delta = nextPoint - currentPosition;
+
+				if (!delta.Equals(lastDirection))
+				{
+					count++;
+					lastDirection = delta;
+				}
+
+				currentPosition = nextPoint;
+
+			} while (!currentPosition.Equals(startPosition));
+
+			//If you don't start in the corner of an edge loop you'll count the same piece twice.
+			//Now every area always HAS an even amount of perimeters so if it is uneven, we counted one too many
+			if (count % 2 == 1) count--;
+			totalPerimeter += count;
+		}
+
+		return totalPerimeter;
+	}
+
+	public void PrintEdgeMap ()
+	{
+		foreach (var kv in graph)
+		{
+            Console.WriteLine(kv.Key);
+			foreach (var edge in kv.Value)
+			{
+				Console.WriteLine("\t" + edge);
+			}
+        }
+	}
+
+}
+

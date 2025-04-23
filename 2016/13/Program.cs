c026107077b26e8ff84752fb4480fe52288ab1e4
innerdrivestudios <info@innerdrivestudios.com>
2025-04-23 21:25:16 +0200

    Updated 2016/13

--- 2016/13/Program.cs
+++ 2016/13/Program.cs
@@ -3,133 +3,117 @@
 using System.Numerics;
 using Vec2i = Vec2<int>;
 
-//Your input: a seed used to determine whether an x,y position in a grid is walkable or not
+// In visual studio you can modify the input used by going to
+// Debug/Debug Properties and changing the command line arguments.
+// This value given will be passed to the built-in args[0] variable.
 
-ulong myInput = 1352;
+// ** Your input: a seed used to determine whether an x,y position in a grid is walkable or not
 
-//Your task : find the minimum number of steps to get from 1,1 to 31,39
+ulong myInput = ulong.Parse(args[0]);
 
-//Start and end poulong
-Vec2i start = new Vec2i(1, 1);
-Vec2i end = new Vec2i(31, 39);
+// ** Your task : find the minimum number of steps to get from grid cell 1,1 to cell 31,39
 
-//Allowed directions
+// Start and end positions
+
+Vec2i start = new Vec2i (1, 1);
+Vec2i end = new Vec2i (31, 39);
+
+// Allowed directions
+
 Vec2i[] directions = [new Vec2i(1, 0), new Vec2i(-1, 0), new Vec2i(0, 1), new Vec2i(0, -1)];
 
-//Part 1 - Assuming start and end are both walkable!
+// ** Part 1 - Assuming start and end are both walkable!
 Console.WriteLine(
-	"Part 1 - Finding the fewest steps required from " + start + " to " + end + " :" +
-	GetFewestStepsCount(start, end, myInput)
+	"Part 1 - Finding the fewest steps required from " + start + " to " + end + ": " +
+	BFS (start, end, myInput)
 );
 
-//Part 2 - Flood fill as long as cost is < 51
+// ** Part 2 - Flood fill as long as cost is < 51
 Console.WriteLine(
-	"Part 2 - Reachable locations from " + start + " in 50 steps :" +
+	"Part 2 - Reachable locations from " + start + " in 50 steps: " +
 	Saturate (start, myInput, 50)
 );
 
-Console.ReadKey();
-
 //Basic Dijkstra/BFS
-int GetFewestStepsCount (Vec2i pStart, Vec2i pEnd, ulong pInput)
+int BFS (Vec2i pStart, Vec2i pEnd, ulong pSeed)
 {
-	PriorityQueue<Vec2i, int> todoList = new();
+	Queue<(Vec2i pos, int cost)> todoList = new();
 	HashSet<Vec2i> visited = new();
 
-	todoList.Enqueue(pStart,0);
+	todoList.Enqueue((pStart,0));
 	visited.Add(pStart);
 
 	while (todoList.Count > 0)
 	{
-		todoList.TryPeek(out _, out int currentCost);
-		Vec2i current = todoList.Dequeue();
+		var current = todoList.Dequeue();
 
-		if (current == pEnd) return currentCost;
+		if (current.pos == pEnd) return current.cost;
 
 		foreach (Vec2i direction in directions) {
-			Vec2i newPosition = current + direction;
+			Vec2i newPosition = current.pos + direction;
 
 			if (newPosition.X < 0 || newPosition.Y < 0) continue;
-			if (visited.Contains(newPosition)) continue;
 
-			//Find x*x + 3*x + 2*x*y + y + y*y
-			//Add your puzzle input
-			//Count the bits
-			//even number of 1? walkable is true;
+            if (visited.Contains(newPosition) || !IsWalkable(pSeed, newPosition)) continue;
 
-			ulong magicNumber =
-				(ulong)(
-				newPosition.X * newPosition.X +
-				3 * newPosition.X +
-				2 * newPosition.X * newPosition.Y +
-				newPosition.Y +
-				newPosition.Y * newPosition.Y
-				);
-				
-			magicNumber += pInput;
-
-			bool isWalkable = BitOperations.PopCount(magicNumber) % 2 == 0;
-
-            if (isWalkable)
-			{
-				todoList.Enqueue(newPosition, currentCost+1);
-				visited.Add(newPosition);
-            }
+            todoList.Enqueue((newPosition, current.cost + 1));
+            visited.Add(newPosition);
 		}
 	}
 	return 0;
 }
 
 //FloodFill with cost tracking
-int Saturate(Vec2i pStart, ulong pInput, int pMaxCost)
+int Saturate(Vec2i pStart, ulong pSeed, int pMaxCost)
 {
-	PriorityQueue<Vec2i, int> todoList = new();
-	HashSet<Vec2i> visited = new();
+    Queue<(Vec2i pos, int cost)> todoList = new();
+    HashSet<Vec2i> visited = new();
 
-	todoList.Enqueue(pStart, 0);
-	visited.Add(pStart);
+    todoList.Enqueue((pStart, 0));
+    visited.Add(pStart);
 
-	while (todoList.Count > 0)
-	{
-		todoList.TryPeek(out _, out int currentCost);
-		Vec2i current = todoList.Dequeue();
+	int visitedCount = 0;
 
-		//from start to next is cost 1 => 1 step, so cost 50 is 50 steps (aka done)
-		if (currentCost >= 50) continue;
+    while (todoList.Count > 0)
+    {
+        var current = todoList.Dequeue();
 
-		foreach (Vec2i direction in directions)
-		{
-			Vec2i newPosition = current + direction;
+		if (current.cost <= 50) visitedCount++;
+		else break;
 
-			if (newPosition.X < 0 || newPosition.Y < 0) continue;
-			if (visited.Contains(newPosition)) continue;
+        foreach (Vec2i direction in directions)
+        {
+            Vec2i newPosition = current.pos + direction;
 
-			//Find x*x + 3*x + 2*x*y + y + y*y
-			//Add your puzzle input
-			//Count the bits
-			//even number of 1? walkable is true;
+            if (newPosition.X < 0 || newPosition.Y < 0) continue;
 
-			ulong magicNumber =
-				(ulong)(
-				newPosition.X * newPosition.X +
-				3 * newPosition.X +
-				2 * newPosition.X * newPosition.Y +
-				newPosition.Y +
-				newPosition.Y * newPosition.Y
-				);
+            if (visited.Contains(newPosition) || !IsWalkable(pSeed, newPosition)) continue;
 
-			magicNumber += pInput;
+            todoList.Enqueue((newPosition, current.cost + 1));
+            visited.Add(newPosition);
+        }
+    }
+    return visitedCount;
+}
 
-			bool isWalkable = BitOperations.PopCount(magicNumber) % 2 == 0;
+bool IsWalkable (ulong pSeed, Vec2i pPosition)
+{
+    //Find x*x + 3*x + 2*x*y + y + y*y
+    //Add your puzzle input
+    //Count the bits
+    //even number of 1? walkable is true;
 
-			if (isWalkable)
-			{
-				todoList.Enqueue(newPosition, currentCost + 1);
-				visited.Add(newPosition);
-			}
-		}
-	}
+    ulong magicNumber =
+        (ulong)(
+        pPosition.X * pPosition.X +
+        3 * pPosition.X +
+        2 * pPosition.X * pPosition.Y +
+        pPosition.Y +
+        pPosition.Y * pPosition.Y
+        );
 
-	return visited.Count;
+    magicNumber += pSeed;
+
+    return BitOperations.PopCount(magicNumber) % 2 == 0;
 }
 

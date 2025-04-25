6c627def486d49da025441108fab787138e9c990
innerdrivestudios <info@innerdrivestudios.com>
2025-04-25 16:49:40 +0200

    Added 2018/13

---
+++ 2018/13/Cart.cs
@@ -0,0 +1,76 @@
+﻿using Vec2i = Vec2<int>;
+
+class Cart
+{
+    public Vec2i position { get; private set; }
+
+    private static string directionChars = ">v<^";
+    
+    private Directions<Vec2i> directions = new ([new(1,0), new(0,1), new(-1,0), new(0,-1)]);
+    private int intersectionsCrossed = 0;
+    
+    private Vec2i originalPosition;
+    private int originalDirection;
+
+    public Cart (Vec2i pPosition, char pDirection)
+    {
+        position = pPosition;
+        directions.index = directionChars.IndexOf(pDirection);
+
+        originalPosition = pPosition;
+        originalDirection = directions.index;
+    }
+
+    public void Reset()
+    {
+        position = originalPosition;
+        directions.index = originalDirection;
+        intersectionsCrossed = 0;
+    }
+
+    public void Move (Grid<char> pTracks)
+    {
+        position += directions.Current();
+
+        //now what do we do next?
+        //there are only 3 special cases:
+        if (pTracks[position] == '+') HandleIntersection();
+        else if (pTracks[position] == '/') HandleForwardSlashTurn();
+        else if (pTracks[position] == '\\') HandleBackwardSlashTurn();
+        //else just enjoy the track :)
+    }
+
+    private void HandleIntersection()
+    {
+        //0 is left (index += - 1), 1 is straight (index += 0), 2 is right (index += 1)
+        directions.index += (intersectionsCrossed % 3) - 1;
+        intersectionsCrossed++;
+    }
+
+    private void HandleForwardSlashTurn()
+    {
+        // How to handle /?
+        // We can approach from 4 directions:
+        // - moving to the right -> turn left
+        // - moving to the left -> turn left
+        // - moving to down -> turn right
+        // - moving to up -> turn right
+
+        if (directions.Current().X != 0) directions.index--;
+        else directions.index++;
+    }
+
+    private void HandleBackwardSlashTurn()
+    {
+        // How to handle \?
+        // We can approach from 4 directions:
+        // - moving to the right -> turn right
+        // - moving to the left -> turn right
+        // - moving to down -> turn left
+        // - moving to up -> turn left
+
+        if (directions.Current().X != 0) directions.index++;
+        else directions.index--;
+    }
+}
+
---
+++ 2018/13/Program.cs
@@ -0,0 +1,120 @@
+﻿//Solution for https://adventofcode.com/2018/day/13 (Ctrl+Click in VS to follow link)
+
+using Vec2i = Vec2<int>;
+
+// In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties
+// and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable
+
+// ** Your input: a list of plants and their replacement rules
+
+string myInput = File.ReadAllText(args[0]).ReplaceLineEndings(Environment.NewLine);
+
+// First create the track and carts
+
+List<Cart> carts = new List<Cart>();
+Grid<char> railroadTracks = new Grid<char>(myInput, Environment.NewLine, null, ProcessTrack);
+
+char ProcessTrack (Vec2i pPosition, string pContent)
+{
+    char content = pContent[0];
+
+    if (content == '^' || content == 'v')
+    {
+        //Create the cart, overwrite the contents with right type of track
+        carts.Add(new Cart(pPosition, content));
+        content = '|';
+    }
+    else if (content == '<' || content == '>')
+    {
+        //Create the cart, overwrite the contents with right type of track
+        carts.Add(new Cart(pPosition, content));
+        content = '-';
+    }
+
+    return content;
+}
+
+Console.WriteLine(carts.Count + " carts found...");
+
+// Now we have all carts... time to simulate them driving down the track
+// and tracking their collisions! 
+
+// Let's define some helper methods first:
+
+int SortAccordingToPosition(Cart pCartA, Cart pCartB)
+{
+    if (pCartA.position.Y == pCartB.position.Y) return pCartA.position.X.CompareTo(pCartB.position.X);
+    else return pCartA.position.Y.CompareTo(pCartB.position.Y);
+}
+
+Vec2i SimulateTrackMovementUntilCollision()
+{
+    //Start with all the current positions...
+    HashSet<Vec2i> cartPositions = carts.Select(x => x.position).ToHashSet();
+
+    while (true)
+    {
+        carts.Sort(SortAccordingToPosition);
+
+        foreach (Cart cart in carts)
+        {
+            //Update the position for each cart, checking if a collision happens
+            cartPositions.Remove(cart.position);
+            //Move the cart and add its new position back into the cartPositions list
+            //If that fails, we have a collision...
+            cart.Move(railroadTracks);
+            if (!cartPositions.Add(cart.position)) return cart.position;
+        }
+    }
+}
+
+Console.WriteLine("Part 1: " + SimulateTrackMovementUntilCollision());
+
+// ** Part 2: What is the location of the last cart at the end of the first
+// tick where it is the only cart left?
+
+foreach (Cart cart in carts) cart.Reset();
+
+Vec2i SimulateTrackMovementUntilThereIsOneCartLeft()
+{
+    //Start with all the current positions mapped to each cart ...
+    Dictionary<Vec2i, Cart> cartPositions2Cart = carts.ToDictionary(x => x.position);
+    HashSet<Cart> stillInTheRace = new HashSet<Cart>(carts);
+
+    while (true)
+    {
+        carts.Sort(SortAccordingToPosition);
+
+        foreach (Cart cart in carts)
+        {
+            if (!stillInTheRace.Contains(cart)) continue;
+
+            Vec2i oldPosition = cart.position;
+            cart.Move(railroadTracks);
+
+            if (cartPositions2Cart.ContainsKey(cart.position))
+            {
+                //Remove the cart at the new position
+                stillInTheRace.Remove(cartPositions2Cart[cart.position]);
+                //Remove ourselves
+                stillInTheRace.Remove(cart);
+                //Remove both of them from the dictionary as well
+                cartPositions2Cart.Remove(oldPosition);
+                cartPositions2Cart.Remove(cart.position);
+                Console.WriteLine("Removed 2 carts, " + stillInTheRace.Count + " carts left.");
+            }
+            else
+            {
+                cartPositions2Cart.Remove(oldPosition);
+                cartPositions2Cart.Add(cart.position, cart);
+            }
+        }
+    
+        //update the list of carts for the next round
+        if (stillInTheRace.Count == 1) return stillInTheRace.First().position;
+        else if (carts.Count != stillInTheRace.Count) carts = stillInTheRace.ToList();
+    }
+}
+
+Console.WriteLine("Part 2: " + SimulateTrackMovementUntilThereIsOneCartLeft());

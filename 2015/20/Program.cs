d02ef5707308f3d9762cea907c2a1b0a876084d4
innerdrivestudios <info@innerdrivestudios.com>
2025-10-13 19:41:18 +0200

    Updated 2015/19 with documentation and commandline args

--- 2015/20/Program.cs
+++ 2015/20/Program.cs
@@ -1,22 +1,30 @@
-﻿//Solution for https://adventofcode.com/2015/day/20 (Ctrl+Click in VS to follow link)
+﻿// Solution for https://adventofcode.com/2015/day/20 (Ctrl+Click in VS to follow link)
 
-//Your input: the number of gifts we want to have at a specific house
-int myInput = 29000000;
+// In visual studio you can modify what input will be used by going to Debug/Debug Properties
+// and specifying the value as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable.
 
-//Your task: given the weird behaviour of the present elves (see day description),
-//figure out what the first house is to have more than the given amount of presents
+// ** Your input: the number of gifts we want to have at a specific house
 
-Console.WriteLine(Part1_LowestHouseNumberToReceiveThisManyPresents(myInput));
-Console.WriteLine(Part2_SameRequestButWithDifferentNumbers(myInput));
+int myInput = int.Parse(args[0]);
+
+// ** Part 1: Given the weird behaviour of the present elves (see day description),
+// figure out what the first house is to have more than the given amount of presents
 
 int Part1_LowestHouseNumberToReceiveThisManyPresents(int pInput)
 {
-    //what is the upper limit of houses to check? 
-    //each elf delivers 10 times its own number so elf (pInput/10) delivers the required amount of presents at house pInput
-    //BUT.... we are probably done WAY before that because of all the other elves also dropping presents
+    // What is the upper limit of houses to check? 
+
+    // Each elf delivers 10 times its own number to each house number of which the elf number is a factor,
+    // e.g. Elf 1 delivers 10 packages at House 1, 2, 3, 4, 5
+    //      Elf 2 delivers 20 packages at House 2, 4, 6, 8, 10
+    // Etc
+    //
+    // So elf (pInput/10) delivers the required amount of presents at house pInput BUT....
+    // we are probably done WAY before that because of all the other elves also dropping presents there
     int[] houses = new int[(pInput / 10)];
 
-    //now for each elf...
+    // Now for each elf... (turns out this is actually extremely fast...)
     for (int elf = 1; elf < houses.Length; elf++)
     {
         //make the elf drops its presents at each house
@@ -31,6 +39,10 @@ int Part1_LowestHouseNumberToReceiveThisManyPresents(int pInput)
 
     return -1;
 }
+
+Console.WriteLine("Part 1: " + Part1_LowestHouseNumberToReceiveThisManyPresents(myInput));
+
+// ** Part 2: Same request but with different numbers...
 
 int Part2_SameRequestButWithDifferentNumbers(int pInput)
 {
@@ -53,5 +65,7 @@ int Part2_SameRequestButWithDifferentNumbers(int pInput)
 
     return -1;
 }
+
+Console.WriteLine("Part 2: "+ Part2_SameRequestButWithDifferentNumbers(myInput));
 
 

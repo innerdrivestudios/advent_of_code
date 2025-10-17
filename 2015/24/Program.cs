f8ea1968cdbab7bd150d0889e93263ca03ea5a25
innerdrivestudios <info@innerdrivestudios.com>
2025-10-17 07:14:03 +0200

    Added/refactored 2015/24

--- 2015/24/Program.cs
+++ 2015/24/Program.cs
@@ -1,51 +1,53 @@
-﻿//Solution for https://adventofcode.com/2015/day/24 (Ctrl+Click in VS to follow link)
+﻿// Solution for https://adventofcode.com/2015/day/24 (Ctrl+Click in VS to follow link)
 
-//Input: list of package weights
+// In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties
+// and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable
 
-string myInput = "1\r\n3\r\n5\r\n11\r\n13\r\n17\r\n19\r\n23\r\n29\r\n31\r\n37\r\n41\r\n43\r\n47\r\n53\r\n59\r\n67\r\n71\r\n73\r\n79\r\n83\r\n89\r\n97\r\n101\r\n103\r\n107\r\n109\r\n113";
-//string myInput = "11\r\n10\r\n9\r\n8\r\n7\r\n5\r\n4\r\n3\r\n2\r\n1";
+// ** Input: list of package weights
+string myInput = File.ReadAllText(args[0]).ReplaceLineEndings();
 
-//Task: create 3 different groups of packages of equal weights, with group 1 having the least amount of packages
-
-//First get all the numbers and make sure they are sorted to speed up the algorithm
-List<int> packages = myInput.Split("\r\n", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+// First get all the numbers and make sure they are sorted to speed up the algorithm
+List<int> packages = myInput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
 packages.Sort((a,b) => b-a);
+
+// ** Part 1 & 2: Create x different groups of packages of equal weights, with the first group having the least amount of packages
 
 Console.WriteLine("Part 1: Quantum entanglement of the ideal config:" + CalculateIdealQuantumEntanglement(packages, 3));
 Console.WriteLine("Part 2: Quantum entanglement of the ideal config:" + CalculateIdealQuantumEntanglement(packages, 4));
 
-Console.ReadKey();
+// Helper methods...
 
 long CalculateIdealQuantumEntanglement(List<int> pPackages, int pNumberOfGroups)
 {
-    //Getting all permutations and testing those takes too much time, so we use a smarter approach
+    // Getting all permutations and testing those takes too much time, so we use a smarter approach (see the worked_example.txt)
+    // Note that this method doesn't actually split the packages into x groups, it just generates all possible configurations
+    // of groups with a weight of total / number of groups (we don't need more than that)
     List<List<int>> possibleConfigurations = new List<List<int>>();
     GetPossibleConfigurationsOfWeightX(pPackages, pPackages.Sum() / pNumberOfGroups, possibleConfigurations);
 
-    //Find the minimum package length
+    // Find the minimum package length of these configs
     int minPackageLength = possibleConfigurations.Min (a => a.Count);
     possibleConfigurations = possibleConfigurations.Where (a => a.Count == minPackageLength).ToList();
 
-    //Find optimum quantum entanglement score ;)
+    // And calculate the optimum quantum entanglement score ;)
     long optimumQuantumScore = possibleConfigurations.Min(a => CalculateQuantumEntanglement(a));
 
 	return optimumQuantumScore;
 }
 
-//also see the worked_example.txt
-//This method expects a descending sorted list of package weights, so it can go through all the provided
-//packages in an order fashion in order to try and establish the requested weight.
+
+// This method expects a descending sorted list of package weights, so it can go through all the provided
+// packages in an order fashion in order to try and establish the requested weight.
 
 void GetPossibleConfigurationsOfWeightX (List<int> pPackages, int pWeight, List<List<int>> pFoundConfigurations, List<int> pConfigurationSoFar = null)
 {
     pConfigurationSoFar = pConfigurationSoFar ?? new List<int>();
 
-    //See if we can even make the requested configuration by combining everything so far, 
-    //otherwise do an early exit
+    //See if we can even make the requested configuration by combining everything so far, otherwise do an early exit
     int maxWeight = pPackages.Sum() + pConfigurationSoFar.Sum();
     if (maxWeight < pWeight) return;
 
-    //Console.WriteLine(String.Join(" ", pConfigurationSoFar));
     //Ok, so in theory by combining items from pPackages and pConfigurationSoFar we should be able to
     //get to pWeight, the question is, can we get to it EXACTLY
 

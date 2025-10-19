52e89a0660b84ae87a2792fa275707da12a598af
innerdrivestudios <info@innerdrivestudios.com>
2025-10-19 17:31:27 +0200

    Added 2017/23

---
+++ 2017/23/Program.cs
@@ -0,0 +1,67 @@
+﻿//Solution for https://adventofcode.com/2017/day/23 (Ctrl+Click in VS to follow link)
+
+// In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties
+// and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable
+
+// ** Your input: a list of instructions ...
+
+string[] myInput = File.ReadAllLines(args[0]);
+string[][] instructions = myInput.Select(x => x.Split(' ')).ToArray();
+
+// ** Part 1: How many times in mul executed?
+
+SimpleVM machine1 = new SimpleVM(instructions);
+
+while (true)
+{
+    SimpleVM.VMState machine1State = machine1.Run();
+
+    if (machine1State != SimpleVM.VMState.Stepped) break;
+}
+
+Console.WriteLine("Part 1:" + machine1.GetInstructionCount("mul"));
+
+// ** Part 2: What is the value in register h?
+
+// Check the xls, if we reverse engineer the program, we can see we are testing
+// d * e for every combination of d and e from 2 to b
+// If d * e, we set f to zero
+// If f is zero we increase h
+// In other words, we count the number of numbers that can be factorized between b and c
+
+// First lets get values b and c, these should be set, once f is set to 1
+
+SimpleVM machine2 = new SimpleVM(instructions);
+machine2.SetRegisterValue("a", 1);
+//machine2.log = true;
+
+while (true)
+{
+
+    SimpleVM.VMState machine2State = machine2.Run();
+
+    if (machine2State != SimpleVM.VMState.Stepped || machine2.GetRegisterValue("f") != 0) break;
+}
+
+long b = machine2.GetRegisterValue("b");
+long c = machine2.GetRegisterValue("c");
+
+int nonPrimeCount = 0;
+
+for(long i = b; i <= c; i += 17)
+{
+    nonPrimeCount += IsPrime(i) ? 0 : 1;
+}
+
+bool IsPrime (long pValue)
+{
+    long top = (long)Math.Ceiling(Math.Sqrt(pValue));
+    for (int i = 2; i <= top;i++)
+    {
+        if (pValue % i == 0) return false;
+    }
+    return true;
+}
+
+Console.WriteLine("Part 2:" + nonPrimeCount);
---
+++ 2017/23/SimpleVM.cs
@@ -0,0 +1,103 @@
+﻿using System.Diagnostics;
+
+class SimpleVM {
+
+    public enum VMState { Stepped, Ended, Waiting };
+
+    private long instructionPointer = 0;
+    private Dictionary<string, long> registers = new();
+    private string[][] instructions;
+
+    private Dictionary<string, int> count = new();
+
+    public bool log = false;
+
+    public SimpleVM(string[][] pInstructions) {
+        instructions = pInstructions;
+
+        foreach (char c in "abcdefgh") registers[""+c] = 0;
+    }
+
+    public VMState Run() {
+        if (instructionPointer >= instructions.Length) return VMState.Ended;
+        
+        var instruction = instructions[instructionPointer];
+        string opcode= instruction[0];
+
+        count[opcode] = count.GetValueOrDefault(opcode, 0) + 1;
+
+        VMState returnValue = VMState.Ended;
+
+        if (log) Console.Write($"Executing [{instructionPointer}] -> {string.Join(" ", instruction)}\t");
+
+        switch (opcode)
+        {
+            case "set": HandleSet(instruction); returnValue = VMState.Stepped; break;
+            case "sub": HandleSub(instruction); returnValue = VMState.Stepped; break;
+            case "mul": HandleMul(instruction); returnValue = VMState.Stepped; break;
+            case "mul1": HandleMul(instruction); returnValue = VMState.Stepped; break;
+            case "mul2": HandleMul(instruction); returnValue = VMState.Stepped; break;
+            case "jnz": HandleJNZ(instruction); returnValue = VMState.Stepped; break;
+        }
+
+        if (log)
+        {
+            foreach (var kv in registers) Console.Write(kv.Key + "=" + ("" + kv.Value).PadRight(10, ' '));
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        return returnValue;
+    }
+
+    // And all the helper methods ...
+
+    void HandleSet(string[] pInstruction)
+    {
+        registers[pInstruction[1]] = GetValue(pInstruction[2]);
+        instructionPointer++;
+    }
+
+    void HandleSub(string[] pInstruction)
+    {
+        registers[pInstruction[1]] -= GetValue(pInstruction[2]);
+        instructionPointer++;
+    }
+
+    void HandleMul(string[] pInstruction)
+    {
+        registers[pInstruction[1]] *= GetValue(pInstruction[2]);
+        instructionPointer++;
+    }
+
+    void HandleJNZ(string[] pInstruction)
+    {
+        long valueX = GetValue(pInstruction[1]);
+        long valueY = GetValue(pInstruction[2]);
+
+        if (valueX != 0) instructionPointer += valueY;
+        else instructionPointer++;
+    }
+
+    long GetValue(string pInput)
+    {
+        if (long.TryParse(pInput, out var value)) return value;
+        else return registers.GetValueOrDefault(pInput, 0);
+    }
+
+    public int GetInstructionCount (string pOpcode)
+    {
+        return count[pOpcode];
+    }
+
+    public void SetRegisterValue (string pRegister, long pValue)
+    {
+        registers[pRegister] = pValue;
+    }
+
+    public long GetRegisterValue(string pRegister)
+    {
+        return registers[pRegister];
+    }
+
+}

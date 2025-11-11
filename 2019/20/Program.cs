477dbe667edc2aba21e09d70125360f905715894
innerdrivestudios <info@innerdrivestudios.com>
2025-11-11 20:35:42 +0100

    Added 2019/20

--- 2019/20/Program.cs
+++ 2019/20/Program.cs
@@ -14,8 +14,8 @@ string myInput = File.ReadAllText(args[0]).ReplaceLineEndings();
 
 Grid<char> dungeon = new Grid<char>(myInput, Environment.NewLine);
 
-// Locate potential portals, note all the characters for portals are added in
-// top to bottom, left to right order
+// Locate potential portal by looking at UPPER CASE characters:
+// Note all the characters for portals are added in top to bottom, left to right order
 
 List<Vec2i> potentialPortals = new();
 dungeon.Foreach(
@@ -30,7 +30,8 @@ dungeon.Foreach(
 	}
 );
 
-// Process potential portals, searching only down and right
+// Process potential portals, searching only down and right,
+// since due to the way we added the potential portals we added them in top to bottom, left to right order
 
 Vec2i[] directions = [new(1, 0), new(0, 1)];
 Dictionary<string, List<Vec2i>> portalCoordinates = new();
@@ -79,6 +80,7 @@ while (potentialPortals.Count > 0)
 }
 
 // Almost done setting up, now we need to connect the portal positions to eachother:
+// In other words, we don't really need to know WHICH portal is WHICH
 
 Dictionary<Vec2i, Vec2i> portalLinks = new();
 
@@ -97,7 +99,7 @@ Vec2i end = portalCoordinates["ZZ"][0];
 
 // End run the basic dijkstra search...
 
-int GetShortestPathCost ()
+int GetShortestPathCostPart1 ()
 {
 	Queue<Vec2i> queue = new Queue<Vec2i>();
 	queue.Enqueue(start);
@@ -141,5 +143,104 @@ int GetShortestPathCost ()
 	return -1;
 }
 
+Console.WriteLine("Part 1: " + GetShortestPathCostPart1());
 
-Console.WriteLine("Part 1: " + GetShortestPathCost());
+// ** Part 2: We have to CO(DE) deeper :) Brilliant :).
+
+// So we'll need to know which portals are on the outside and which portals are on the inside...
+
+// Well, actually, we only need to know if a portal is on the inside, because if it is not,
+// it is on the outside!
+
+HashSet<Vec2i> insidePortals = new();
+
+// We can check this by looking at the distance from the center between two portals;
+// the one closest will be an inner portal, the furthest an outer portal
+
+Vec2i center = new Vec2i(dungeon.width / 2, dungeon.height / 2);
+
+foreach (var portal in portalCoordinates)
+{
+	// If the portal doesn't have 1 entry and 1 exit skip
+	if (portal.Value.Count != 2) continue;
+
+	// Now figure out which portal is closest to the center...
+	Vec2i coord1 = portal.Value[0];
+	Vec2i coord2 = portal.Value[1];
+
+	if ((coord1 - center).MaxAbsCoord() < (coord2 - center).MaxAbsCoord())
+	{
+		insidePortals.Add (coord1);
+	}
+	else
+	{
+		insidePortals.Add (coord2);
+	}
+}
+
+// With this info we can rewrite our path finding mechanism
+
+
+int GetShortestPathCostPart2()
+{
+	// Search elements are now 
+	Queue<(Vec2i position, int level)> queue = new ();
+
+	(Vec2i position, int level) startNode = (start, 0);
+
+    queue.Enqueue(startNode);
+	
+	// Set up cost table 
+	Dictionary<(Vec2i, int), int> costs = new ();
+	costs[startNode] = 0;
+
+	Vec2i[] directions = [new(-1, 0), new(0, -1), new(1, 0), new(0, 1)];
+
+	while (queue.Count > 0)
+	{
+		(Vec2i, int) current = queue.Dequeue();
+		if (current == (end, 0)) return costs[current];
+
+		Vec2i currentPosition = current.Item1;
+		int currentLevel = current.Item2;
+		int currentCost = costs[current];
+		int newCost = currentCost + 1;
+
+		// First check the normal directions
+		foreach (var direction in directions)
+		{
+			Vec2i newPosition = currentPosition + direction;
+			if (dungeon[newPosition] != '.') continue;
+
+			//Already visited
+			if (costs.ContainsKey((newPosition, currentLevel))) continue;
+
+			costs[(newPosition, currentLevel)] = newCost;
+			queue.Enqueue((newPosition, currentLevel));
+		}
+
+		// Now check whether there is a portal we can jump to:
+		if (portalLinks.ContainsKey(currentPosition))
+		{
+			if (currentLevel == 0 && !insidePortals.Contains(currentPosition)) continue;
+
+			Vec2i newPosition = portalLinks[currentPosition];
+			int newLevel = currentLevel + (insidePortals.Contains(currentPosition) ? 1 : -1);
+
+			var newNode = (newPosition, newLevel);
+
+			//Already visited
+			if (costs.ContainsKey(newNode)) continue;
+
+			costs[newNode] = newCost;
+			queue.Enqueue(newNode);
+		}
+	}
+
+	return -1;
+}
+
+Console.WriteLine("Part 2: " + GetShortestPathCostPart2());
+
+// Build a cache table from all outside portals to all outside portals going 1 up (-1)
+// Build a cache table from all inside to all inside portals stay 

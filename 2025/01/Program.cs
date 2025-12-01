a620f2e5b1a0ea9f4f3b74fd46cd9bda9203984f
innerdrivestudios <info@innerdrivestudios.com>
2025-12-01 15:12:56 +0100

    Added docs to 2025/01

--- 2025/01/Program.cs
+++ 2025/01/Program.cs
@@ -4,22 +4,27 @@
 // and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
 // This value will be processed and passed to the built-in args[0] variable
 
-// ** Your input: ....
+// ** Your input: a list of Left and Right turn instructions...
 
-string myInput = File.ReadAllText(args[0]);
-myInput = myInput.Replace("R", "+");
-myInput = myInput.Replace("L", "-");
-myInput = myInput.ReplaceLineEndings(Environment.NewLine);
+// Parse the numbers
 
-int[] numbers = myInput
+int[] numbers = 
+    File.ReadAllText(args[0])
+    .Replace("R", "+")
+    .Replace("L", "-")
+    .ReplaceLineEndings(Environment.NewLine)
     .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
     .Select(int.Parse)
     .ToArray();
+
+// Define a helper method to modulo the input both positively as negatively...
 
 int Wrap (int pInput, int pWrapValue)
 {
     return (((pInput % pWrapValue) + pWrapValue) % pWrapValue);
 }
+
+// ** Part 1:
 
 int startValue = 50;
 int zeroCount = 0;
@@ -32,6 +37,8 @@ foreach (int number in numbers)
 
 Console.WriteLine("Part 1: " + zeroCount);
 
+// ** Part 2:
+
 startValue = 50;
 zeroCount = 0;
 foreach (int number in numbers)
@@ -42,7 +49,6 @@ foreach (int number in numbers)
         startValue = Wrap(startValue, 100);
         if (startValue == 0) zeroCount++;
     }
-
 }
 
 Console.WriteLine("Part 2: " + zeroCount);

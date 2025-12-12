4f1e8fe3abafd9bf767b231fc8c6ab2986ef8350
innerdrivestudios <info@innerdrivestudios.com>
2025-12-12 07:40:37 +0100

    Optimized 2025/09

--- 2025/09/Program.cs
+++ 2025/09/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Solution for https://adventofcode.com/2025/day/9 (Ctrl+Click in VS to follow link)
 
-using System.Drawing;
 using Vec2i = Vec2<int>;
 
 // In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties

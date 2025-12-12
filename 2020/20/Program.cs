5b5a79ecd9ff4b9919f3453b43fed5e61f8cf6e1
innerdrivestudios <info@innerdrivestudios.com>
2025-12-12 07:35:52 +0100

    2020 20 wip

---
+++ 2020/20/Program.cs
@@ -0,0 +1,8 @@
+﻿// Solution for https://adventofcode.com/2020/day/20 (Ctrl+Click in VS to follow link)
+
+// In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties
+// and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable
+
+// ** Your input: ...
+

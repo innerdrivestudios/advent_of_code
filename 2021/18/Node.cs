3dc173aa0bf006f69fcfb8672193932341d81f8c
innerdrivestudios <info@innerdrivestudios.com>
2025-12-20 18:05:27 +0100

    Added 2021/18

---
+++ 2021/18/Node.cs
@@ -0,0 +1,139 @@
+﻿class Node
+{
+    public Node parent = null;          
+    public Node leftNode = null;
+    public Node rightNode = null;
+
+    public int value = 0;
+
+    public enum NodeType { Nested, Value, Invalid };
+
+    public NodeType nodeType
+    {
+        get
+        {
+            // If left and right are both null, we are a value node, if either is value but not the other, we have issues...
+            return (leftNode == null && rightNode == null) ?
+                NodeType.Value :
+                (leftNode != null && rightNode != null) ? NodeType.Nested : NodeType.Invalid;
+        }
+    }
+
+    public bool Explode(int pDepth = 0)
+    {
+        // If we represent a nested node with two value children above depth 4, we need to explode...
+        if (pDepth >= 4 && leftNode != null && leftNode.nodeType == NodeType.Value && rightNode != null && rightNode.nodeType == NodeType.Value)
+        {
+            Node leftValueNode = FindLeftValueNode();
+            Node rightValueNode = FindRightValueNode();
+
+            if (leftValueNode != null) leftValueNode.value += leftNode.value;
+            if (rightValueNode != null) rightValueNode.value += rightNode.value;
+
+            // Reset ourselves to a value node with value zero...
+
+            value = 0;
+            leftNode = null;
+            rightNode = null;
+
+            // We exploded, so stop searching ...
+            return true;
+        }
+
+        // If we did not explode yet, try left first and then right...
+        // Note how the setup will only execute right explode if no left explode has triggered
+        return (leftNode != null && leftNode.Explode(pDepth+1)) || (rightNode != null && rightNode.Explode(pDepth+1));
+    }
+
+
+    private Node FindLeftValueNode()
+    {
+        // These kind of methods are hard to understand without drawings, but basically,
+        // if we want the VALUE node next to us, we first traverse upward as long as we are the left branch in our parent
+        Node current = this;
+        while (current.parent != null && current.parent.leftNode == current) current = current.parent;
+
+        // as soon as we are not, we are the right branch and we take our PARENTS left branch ONCE
+        if (current.parent != null) current = current.parent.leftNode; else return null;
+
+        // then seek downward to find a value node...
+        while (current.nodeType != NodeType.Value) current = current.rightNode;
+
+        return current;
+    }
+
+    private Node FindRightValueNode()
+    {
+        // Same as above but then flipped...
+        Node current = this;
+        while (current.parent != null && current.parent.rightNode == current) current = current.parent;
+        if (current.parent != null) current = current.parent.rightNode; else return null;
+        while (current.nodeType != NodeType.Value) current = current.leftNode;
+        return current;
+    }
+
+    // Splitting is easier...
+
+    public bool Split()
+    {
+        // If we are a value node...
+        if (nodeType == NodeType.Value)
+        {
+            //... check if we need to be split
+
+            if (value < 10) return false;
+
+            // Execute the split if needed:
+            int leftValue = (int) Math.Floor(value / 2f);
+            int rightValue = (int) Math.Ceiling(value / 2f);
+
+            leftNode = new Node();
+            leftNode.value = leftValue;
+            leftNode.parent = this;
+
+            rightNode = new Node();
+            rightNode.value = rightValue;
+            rightNode.parent = this;
+
+            return true;
+        }
+        else // check if our children need to be split ...
+        {
+            return leftNode.Split() || rightNode.Split();
+        }
+    }
+
+    // Return a new node that represents adding this + pOther
+    // Note that this causes this and pOther to become part of a larger tree
+    public Node AddNode (Node pOther)
+    {
+        // Create the new node with myself and the other as children
+        Node newNode = new Node();
+        newNode.leftNode = this;
+        newNode.rightNode = pOther;
+
+        // Correct the parents
+
+        this.parent = newNode;
+        pOther.parent = newNode;
+
+        // Reduce before returning !
+
+        while (newNode.Explode() || newNode.Split()) { }
+
+        return newNode;
+    }
+
+    public long Magnitude ()
+    {
+        return nodeType == NodeType.Value ? value : ( 3L * leftNode.Magnitude() + 2L * rightNode.Magnitude() );
+    }
+
+    // Helper method for debugging ...
+    public override string ToString()
+    {
+        if (nodeType == NodeType.Value) return value.ToString();
+        return "[" + leftNode.ToString() + "," + rightNode.ToString() + "]";
+    }
+
+}
---
+++ 2021/18/Program.cs
@@ -0,0 +1,90 @@
+﻿//Solution for https://adventofcode.com/2021/day/18 (Ctrl+Click in VS to follow link)
+
+// In visual studio you can modify what input file will be loaded by going to Debug/Debug Properties
+// and specifying its path and filename as a command line argument, e.g. "$(SolutionDir)input" 
+// This value will be processed and passed to the built-in args[0] variable
+
+// ** Your input: snailfish :)
+
+string[] myInput = File.ReadAllLines(args[0]);
+
+// ** Define some helper methods...
+
+// Define a simple helper method to avoid having to declare an int ref param all the time
+
+Node ParseSnailFishHelper (string pInput)
+{
+    int index = 0;
+    return ParseSnailFishExpression(pInput, ref index);
+}
+
+// Recursively parse a snail fish expression, building a tree of snailfish 'nodes'
+// The startindex is recursively updated as parsing progresses...
+Node ParseSnailFishExpression(string pInput, ref int pStartIndex)
+{
+    // We have [,] nodes and value nodes (e.g. 100)
+    Node node = new Node();
+
+    if (char.IsDigit(pInput[pStartIndex]))
+    {
+        node.value = ParseDigit(pInput, ref pStartIndex);
+    }
+    else
+    {
+        pStartIndex++;                                                                  // skip [
+        node.leftNode = ParseSnailFishExpression(pInput, ref pStartIndex);              // parse left child content
+        pStartIndex++;                                                                  // skip ,
+        node.rightNode = ParseSnailFishExpression(pInput, ref pStartIndex);             // parse right child content
+        pStartIndex++;                                                                  // skip ]
+
+        node.leftNode.parent = node;                                                    // don't forget to assign parents...
+        node.rightNode.parent = node;
+    }
+
+    return node;
+}
+
+// Simple helper method to parse digits with an offset into a string
+
+int ParseDigit (string pInput, ref int pStartIndex)
+{
+    int result = 0;
+    while (char.IsDigit(pInput[pStartIndex]))
+    {
+        result = result * 10 + (pInput[pStartIndex]-'0');
+        pStartIndex++;
+    }
+    return result;
+}
+
+// ** Part 1:
+
+Node node = null;
+
+foreach (string input in myInput)
+{
+    Node nextNode = ParseSnailFishHelper(input);
+    node = node == null ? nextNode : node.AddNode(nextNode);
+}
+
+Console.WriteLine("Part 1: " + node.Magnitude());
+
+// ** Part 2: 
+
+long max = 0;
+
+for (int i = 0; i < myInput.Length; i++)
+{
+    for (int j = 0; j < myInput.Length; j++)
+    {
+        if (i == j) continue;
+
+        //note that node a and b are changed currently by the addnode operation
+        Node a = ParseSnailFishHelper(myInput[i]);
+        Node b = ParseSnailFishHelper(myInput[j]);
+
+        max = long.Max(max, a.AddNode(b).Magnitude());
+    }
+}
+
+Console.WriteLine("Part 2: " + max);
